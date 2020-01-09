e28da6e0e23639242f404c2e0d62f30e36d7b2f6
sunny.sim <sunny.sim@kakaocorp.com>
2020-01-09 17:53:08 +0900

    자바스크립트 100제 46번 재업로드

--- 자바스크립트100제/index.js
+++ 자바스크립트100제/index.js
@@ -357,3 +357,11 @@ let total = 0;
 for(let i = 0; i < 20; i++){
   arr[i] = i + 1;
 }
+
+arr.forEach((n) => {
+  while(n){
+    total = total + n%10;
+    n = Math.floor(n/10)
+  }
+});
+console.log(total);
